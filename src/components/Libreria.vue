09210ebc51c6b06b09c9a717a4ad67f9d453ecf8
Nicolas Carrasco <nico.carrasco.arandia@outlook.com>
2020-06-01 20:17:29 -0400

    develop: correcciones importantes para dar mayor versatilidad a los filtros de búsqueda y los datos fueron movidos a store

--- src/components/Libreria.vue
+++ src/components/Libreria.vue
@@ -26,73 +26,30 @@ export default {
   data () {
     return {
       filtro: '',
-      categoria: '',
-      productos: [
-        {
-          codigo: 1,
-          nombre: 'libro 1',
-          descripcion: 'descripción 1',
-          categoria: 'libros'
-        },
-        {
-          codigo: 2,
-          nombre: 'libro 2',
-          descripcion: 'descripción 2',
-          categoria: 'libros'
-        },
-        {
-          codigo: 3,
-          nombre: 'revista 1',
-          descripcion: 'descripción 3',
-          categoria: 'revistas'
-        },
-        {
-          codigo: 4,
-          nombre: 'Revista 2',
-          descripcion: 'descripción 4',
-          categoria: 'revistas'
-        },
-        {
-          codigo: 5,
-          nombre: 'Periódico 1',
-          descripcion: 'descripción 5',
-          categoria: 'periódicos'
-        },
-        {
-          codigo: 6,
-          nombre: 'Periódico 2',
-          descripcion: 'descripción 6',
-          categoria: 'periódicos'
-        },
-        {
-          codigo: 7,
-          nombre: 'Comic 1',
-          descripcion: 'descripción 7',
-          categoria: 'otros'
-        },
-        {
-          codigo: 8,
-          nombre: 'Comic 2',
-          descripcion: 'descripción 8',
-          categoria: 'otros'
-        }
-      ],
-      categorias: [
-        'libros',
-        'revistas',
-        'periódico',
-        'otros',
-        'todas'
-      ]
+      categoria: 'todas'
     }
   },
   computed: {
     filtrados () {
       if (this.categoria === 'todas') {
-        return this.productos
+        if (this.filtro === '') {
+          return this.productos
+        } else {
+          return this.productos.filter(item => item.nombre === this.filtro)
+        }
       } else {
-        return this.productos.filter(item => item.categoria === this.categoria)
+        if (this.filtro === '') {
+          return this.productos.filter(item => item.categoria === this.categoria)
+        } else {
+          return this.productos.filter(item => item.categoria === this.categoria && item.nombre === this.filtro)
+        }
       }
+    },
+    categorias () {
+      return this.$store.state.categorias
+    },
+    productos () {
+      return this.$store.state.productos
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,63 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    categorias: [
+      'libros',
+      'revistas',
+      'periodico',
+      'otros',
+      'todas'
+    ],
+    productos: [
+      {
+        codigo: 1,
+        nombre: 'libro 1',
+        descripcion: 'descripción 1',
+        categoria: 'libros'
+      },
+      {
+        codigo: 2,
+        nombre: 'libro 2',
+        descripcion: 'descripción 2',
+        categoria: 'libros'
+      },
+      {
+        codigo: 3,
+        nombre: 'revista 1',
+        descripcion: 'descripción 3',
+        categoria: 'revistas'
+      },
+      {
+        codigo: 4,
+        nombre: 'Revista 2',
+        descripcion: 'descripción 4',
+        categoria: 'revistas'
+      },
+      {
+        codigo: 5,
+        nombre: 'Periódico 1',
+        descripcion: 'descripción 5',
+        categoria: 'periodico'
+      },
+      {
+        codigo: 6,
+        nombre: 'Periódico 2',
+        descripcion: 'descripción 6',
+        categoria: 'periodico'
+      },
+      {
+        codigo: 7,
+        nombre: 'Comic 1',
+        descripcion: 'descripción 7',
+        categoria: 'otros'
+      },
+      {
+        codigo: 8,
+        nombre: 'Comic 2',
+        descripcion: 'descripción 8',
+        categoria: 'otros'
+      }
+    ]
   },
   mutations: {
   },
